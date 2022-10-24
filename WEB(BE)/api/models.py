5aabb761a2a583eb0023b3df7841a328a3deb6dd
Park Jeongseop <parkjeongseop@parkjeongseop.com>
2022-10-24 11:23:30 +0000

    개발: SSO사용자 모델 구성 및 모델 일부 수정

--- WEB(BE)/api/models.py
+++ WEB(BE)/api/models.py
@@ -1,4 +1,3 @@
-from ipaddress import ip_address
 from django.db import models
 
 # 영상파일
@@ -11,20 +10,36 @@ class Record(models.Model):
     approved_at = models.DateTimeField(auto_now=True, verbose_name='업뎃 된 날짜')
     unit = models.CharField(null=False, max_length=255, verbose_name='녹화된 부대')
 
+    def __str__(self):
+        return f"{self.file_name} {self.owner} {self.unit}"
+
 
 # 조회기록
 class ViewHistory(models.Model):
     id = models.BigAutoField(primary_key=True, verbose_name='조회기록 ID값')
     viewer = models.CharField(null=False, max_length=20, verbose_name='조회자 군번')
     ip_address = models.CharField(null=False, max_length=255, verbose_name='조회한 IP주소')
-    record_id = models.ForeignKey("Record", related_name="Record-ViewHistory", on_delete=models.CASCADE, verbose_name='녹화영상 ID값')
+    record_id = models.ForeignKey("Record", on_delete=models.CASCADE, verbose_name='녹화영상 ID값')
 
     
 # 특정 파일 조회권한
 class Permission(models.Model):
     id = models.BigAutoField(primary_key=True, verbose_name='조회권한 ID값')
-    record_id = models.ForeignKey("Record", related_name="Record-Permission", on_delete=models.CASCADE, verbose_name='녹화영상 ID값')
+    record_id = models.ForeignKey("Record", on_delete=models.CASCADE, verbose_name='녹화영상 ID값')
     allowed_user = models.CharField(null=False, max_length=20, verbose_name='권한부여받은 유저 군번')
 
 
 # 반출 요청/승인
+
+
+
+
+
+class MOUS(models.Model):
+    id = models.BigAutoField(primary_key=True, verbose_name='MOUS ID값')
+    sn = models.CharField(null=False, max_length=20, verbose_name='군번')
+    rk = models.CharField(null=False, max_length=20, verbose_name='계급')
+    nm = models.CharField(null=False, max_length=20, verbose_name='이름')
+
+    def __str__(self):
+        return f"{self.sn} {self.rk} {self.nm}"
